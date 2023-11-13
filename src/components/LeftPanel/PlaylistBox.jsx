0ba310a2acf0b856b0532f42b661fcd05713447b
Shreyans <103171711+mehtashreyans3602@users.noreply.github.com>
2023-11-14 04:16:50 +0530

    Update PlaylistBox.jsx

--- src/components/LeftPanel/PlaylistBox.jsx
+++ src/components/LeftPanel/PlaylistBox.jsx
@@ -2,8 +2,9 @@
 import '@/components/LeftPanel/PlaylistBox.css';
 import { useState } from "react";
 import CategoryButtons from "./CategoryButtons";
-import { FaPlus } from 'react-icons/fa';
-const PlaylistBox = ({token_data}) => {
+import { FaPlus, FaFolderPlus, FaMusic } from 'react-icons/fa';
+import axios from 'axios';
+const PlaylistBox = ({ token_data }) => {
     const [activeCategory, setActiveCategory] = useState('playlist');
     let token_info = token_data || '';
     const handleButtonClick = (category) => {
@@ -12,36 +13,43 @@ const PlaylistBox = ({token_data}) => {
     };
     return (
         <div className="rounded-md  h-[70vh] bg-[#1C1C21] m-1 ">
-            {!token_data ? (<div className='flex w-full h-full justify-center items-center'><div className='text-2xl text-white justify-center items-center border-white border-2 p-20 rounded-md'>Log In First</div></div>):(<>
-                <div className="w-full flex justify-between p-5 text-slate-200 font-medium text-md items-center">
-                <span>Your Library</span>
-                <span><FaPlus/></span>
-            </div>
-            <div className="flex space-x-8 px-5 overflow-scroll">
-                <CategoryButtons
-                    label="Playlist"
-                    active={activeCategory === 'playlist'}
-                    onClick={() => handleButtonClick('playlist')}
-                />
-                <CategoryButtons
-                    label="Podcasts"
-                    active={activeCategory === 'podcasts'}
-                    onClick={() => handleButtonClick('podcasts')}
-                />
-                <CategoryButtons
-                    label="Albums"
-                    active={activeCategory === 'albums'}
-                    onClick={() => handleButtonClick('albums')}
-                />
-                <CategoryButtons
-                    label="Artists"
-                    active={activeCategory === 'artists'}
-                    onClick={() => handleButtonClick('artists')}
-                />
-            </div>
-            </>
+            {!token_data ? (
+                <></>)
+                : (
+                    <>
+                        <div className="w-full flex justify-between p-5 text-slate-200 font-medium text-md items-center">
 
-            )}
+                            <span className='gap-2 flex items-center'><FaMusic />Your Library</span>
+                            <span><FaPlus /></span>
+                        </div>
+                        <div className="flex space-x-8 px-5 overflow-scroll">
+                            <CategoryButtons
+                                label="Playlist"
+                                active={activeCategory === 'playlist'}
+                                onClick={() => handleButtonClick('playlist')}
+                            />
+                            <CategoryButtons
+                                label="Podcasts"
+                                active={activeCategory === 'podcasts'}
+                                onClick={() => handleButtonClick('podcasts')}
+                            />
+                            <CategoryButtons
+                                label="Albums"
+                                active={activeCategory === 'albums'}
+                                onClick={() => handleButtonClick('albums')}
+                            />
+                            <CategoryButtons
+                                label="Artists"
+                                active={activeCategory === 'artists'}
+                                onClick={() => handleButtonClick('artists')}
+                            />
+                        </div>
+                        <div>
+
+                        </div>
+                    </>
+
+                )}
         </div>
     )
 };
